56bf65c13fe116624a4ce73f5f2edf034c009e06
Kentaro Inomata <kentaro.inomata@gmail.com>
2019-10-21 07:33:06 +0900

    Update electron and vue

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
-'use strict';
+const { app, BrowserWindow } = require('electron')
 
-const electron = require('electron');
-const {app} = electron;
-const {BrowserWindow} = electron;
-const {Menu} = electron;
-const {dialog} = electron;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin')
-    app.quit();
-});
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-app.on('ready', function () {
-  Menu.setApplicationMenu(menu);
-  const win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadURL('file://' + __dirname + '/app/index.html');
-});
+  // and load the index.html of the app.
+  win.loadFile('app/index.html')
 
-const template = [
-  {
-    label: "Application",
-    submenu: [
-      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      { type: "separator" },
-      { label: "Quit", accelerator: "CmdOrCtrl+Q", click: function () { app.quit(); } }
-    ]
-  },
-  {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      { label: 'Reload', accelerator: 'CmdOrCtrl+R', click: function (item, focusedWindow) { if (focusedWindow) focusedWindow.reload(); } },
-      { label: 'Toggle DevTools', accelerator: 'Alt+CmdOrCtrl+I', click: function (item, focusedWindow) { if (focusedWindow) focusedWindow.toggleDevTools(); } }
-    ]
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-];
+})
 
-const menu = Menu.buildFromTemplate(template);
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
